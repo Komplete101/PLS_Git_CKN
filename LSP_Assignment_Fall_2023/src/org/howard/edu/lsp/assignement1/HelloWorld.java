8e62f32613aa737a2cfe599540214723f28bca95
Komplete101 <kylenwosu2003@gmail.com>
2023-08-26 20:21:56 -0400

    Initial commit

---
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assignement1/HelloWorld.java
@@ -0,0 +1,13 @@
+package org.howard.edu.lsp.assignement1;
+
+public class HelloWorld {
+	/**
+	 * Kyle Nwosu
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
