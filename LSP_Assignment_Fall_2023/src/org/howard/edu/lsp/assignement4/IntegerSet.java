3a1cf8e22a6f8e2ce32fa97f06e37759b7a68b80
Komplete101 <106222631+Komplete101@users.noreply.github.com>
2023-10-14 12:42:28 -0400

    Progress

--- LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assignement4/IntegerSet.java
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assignement4/IntegerSet.java
@@ -15,8 +15,7 @@ public class IntegerSet  {
 	}
  	
  	public void clear(ArrayList<Integer> set) {
- 		this.set = new ArrayList<Integer>();
- 		
+ 		this.set = new ArrayList<Integer>();	
  	};
  	
 	public int length(ArrayList<Integer> set) {
@@ -28,18 +27,58 @@ public class IntegerSet  {
 		System.out.println(x);
 	}; 
 	
-	public boolean equals(Object o) {
+	public boolean equals(ArrayList<Integer> set1, ArrayList<Integer> set2) {
+		boolean a = true;
+		Iterator<Integer> search1 = set1.iterator();
+		Iterator<Integer> search2 = set2.iterator();
+		while(search1.hasNext() && (search2.hasNext())){
+			if (search1.next()!=search2.next()) {
+				a=false;
+				break;
+			}
+		}
+		System.out.print(a);
+	};
+	
+	public boolean contains(int value, ArrayList<Integer> set) {
+		boolean a = false;
+		Iterator<Integer> search = set.iterator();
+		while(search.hasNext()) {
+			if (search.next()==value) {
+				 a = true;
+			}
+		System.out.print(a);
+		}
 		
 	};
-	public boolean contains(int value) {
-		
-	};
-	public int largest() {
-		
-	};
-	public int smallest() IntegerSetException {
-		
-	};
+	public int largest(ArrayList<Integer> set) throws IntegerSetException{
+		int large = 0;
+		Iterator<Integer> l = set.iterator();
+		if (set.isEmpty()) {
+			throw new IntegerSetException ("The set is empty") ;
+		}
+		while(l.hasNext()) {
+			if (l.next()>large) {
+				large = l.next();
+			}
+	}
+		System.out.println( large + " is the largest number in this set");
+		return large;
+}
+	
+	public int smallest(ArrayList<Integer> set) throws IntegerSetException {
+		int large = 0;
+		Iterator<Integer> l = set.iterator();
+		if (set.isEmpty()) {
+			throw new IntegerSetException ("The set is empty") ;
+		}
+		while(l.hasNext()) {
+			if (l.next()>large) {
+				large = l.next();
+			}
+	}
+		return large;
+	}
 	
 	public void add(int item,ArrayList<Integer> set) {
 		if (set.contains(item)) {
---
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assignement4/IntegerSetException.java
@@ -0,0 +1,11 @@
+package org.howard.edu.lsp.assignement4;
+
+public class IntegerSetException extends Exception {
+
+	public IntegerSetException (String message) {
+		super(message);
+		
+
+	}
+
+}
